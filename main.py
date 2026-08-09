43efe99052e55b9d17aebe7b0b8f456d21aaf4d2
Tasuba <Brokensky585@gmail.com>
2026-08-09 09:55:22 -0400

    Updated code to match boot.dev final

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from stats import (
-    get_num_words,
-    chars_dict_to_sorted_list,
-    get_chars_dict,
-)
 import sys
 
+from stats import (
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+    get_num_words,
+)
 
-def main():
-    if len(sys.argv) != 2:
+
+def main() -> None:
+    if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-
     book_path = sys.argv[1]
+
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -19,21 +20,23 @@ def main():
     print_report(book_path, num_words, chars_sorted_list)
 
 
-def get_book_text(path):
+def get_book_text(path: str) -> str:
     with open(path) as f:
         return f.read()
 
 
-def print_report(book_path, num_words, chars_sorted_list):
+def print_report(
+    book_path: str, num_words: int, chars_sorted_list: list[tuple[str, int]]
+) -> None:
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    for item in chars_sorted_list:
-        if not item["char"].isalpha():
+    for char, count in chars_sorted_list:
+        if not char.isalpha():
             continue
-        print(f"{item['char']}: {item['num']}")
+        print(f"{char}: {count}")
 
     print("============= END ===============")
 
